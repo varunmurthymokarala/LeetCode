981ac2fddf899441f19913a2eab3b6e392ff7f0b
varunmurthymokarala <mokaralavarun@gmail.com>
2021-06-10 11:22:03 -0400

    O(N) Solution

--- lc01_twosum.java
+++ lc01_twosum.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-             HashMap<Integer, Integer> map = new HashMap();
-        int n = nums.length;
-        int remaining_sum;
-        System.out.println(target);
-        for(int i = 0; i < n; i++){
-            remaining_sum = target - nums[i];
-            
-            if(map.containsKey(remaining_sum)) return new int[]{map.get(remaining_sum), i};
-            else map.put(nums[i], i);
-        }
-        return new int[]{};   
-    } 
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{ i, map.get(target - nums[i])};
+            }  
+            map.put(nums[i], i);
+       
+        }  return new int[0];
+    }
 }
